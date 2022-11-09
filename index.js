ff576c4eadebe2e9a72d64169a6a3fcaf3ee7094
Nahida Zakir <nahidazakir15@gmail.com>
2022-11-10 01:26:48 +0600

    review data added plus link

--- index.js
+++ index.js
@@ -38,9 +38,15 @@ async function run() {
         })
         app.get('/services/:id', async (req, res) => {
             const id = req.params.id;
-            const query = { _id: ObjectId(id) };
-            const service = await serviceCollection.findOne(query);
+            const query1 = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query1);
             res.send(service);
+        })
+        app.get('/reviews', async (req, res) => {
+            const query = {};
+            const cursor = reviewsCollection.find(query);
+            const reviews = await cursor.toArray();
+            res.send(reviews);
         })
     }
 
